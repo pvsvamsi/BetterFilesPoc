9c3546287b4c053875bb425bda5654b968338ed1
pvsvamsi <pvsvamsi2009@gmail.com>
2017-03-24 11:06:44 +0530

    calling from method

--- src/main/scala/FileWatchers.scala
+++ src/main/scala/FileWatchers.scala
@@ -12,22 +12,29 @@ object FileWatchers extends App {
   implicit val system = ActorSystem("mySystem")
   var counter: Int = 0
 
-  try {
-    val watcher: ActorRef = File("/home/venkat/Documents").newWatcher(recursive = true)
-    // register partial function for an event
-    watcher ! on(EventType.ENTRY_DELETE) {
-      case file => println(s"$file got deleted")
-    }
+  def addWatcher( path : String) : Unit = {
+    try {
+      val watcher: ActorRef = File(path).newWatcher(recursive = true)
+      // register partial function for an event
+      watcher ! on(EventType.ENTRY_DELETE) {
+        case file => println(s"$file got deleted")
+      }
 
-    // watch for multiple events
-    watcher ! when(events = EventType.ENTRY_CREATE) {
-      case (EventType.ENTRY_CREATE, file) => {
+      // watch for multiple events
+      watcher ! when(events = EventType.ENTRY_CREATE) {
+        /*case (EventType.ENTRY_CREATE, file) => {
         counter += 1
-        println(s"$file got created and the count files count is "+ counter)}
-      case (EventType.ENTRY_MODIFY, file) => println(s"$file got modified " + file)
+        println(s"$file got created and the count files count is "+ counter)}*/
+        //      case (EventType.ENTRY_MODIFY, file) => println(s"$file got modified " + file)
+        case (et, file) => {
+          counter += 1
+          println(s"$file got created and the count files count is " + counter + " event type is " + et + " is directory " + file.isDirectory)
+        }
+      }
+    } catch {
+      case e: MatchError => println("match error")
     }
-  }catch {
-    case e : MatchError => println("match error")
   }
+  addWatcher("/home/venkat/Documents")
 
 }
